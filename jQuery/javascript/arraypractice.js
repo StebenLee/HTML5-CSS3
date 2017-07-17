137a22d5cdf238ecf659bedb5f99d49cc01eafd6
JirLu <s1365789@gmail.com>
2017-07-17 10:08:00 +0800

    arraypr

---
+++ jQuery/javascript/arraypractice.js
@@ -0,0 +1,13 @@
+var bucketList = ['how','are','you'];
+console.log(bucketList);
+
+var listItem = bucketList[2];
+console.log(listItem);
+
+console.log(bucketList.length);
+
+bucketList.push('item3', 'item4');
+console.log(bucketList);
+
+bucketList.pop()
+console.log(bucketList);
