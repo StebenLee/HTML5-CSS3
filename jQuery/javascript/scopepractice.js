b5cd64c7517e8dae28adac687be4451f1d744c6f
JirLu <s1365789@gmail.com>
2017-07-17 09:45:37 +0800

    scopepractice

---
+++ jQuery/javascript/scopepractice.js
@@ -0,0 +1,35 @@
+var laundryRoom = 'Basement',
+    mailRoom = 'Room 1A';
+
+function myApartment() {
+  var mailBoxNumber = 'Box 3';
+  laundryRoom = 'In-unit';
+  console.log('Mail box:' + mailBoxNumber + ', Laundry: ' + laundryRoom);
+}
+    
+
+console.log('Laundry: ' + laundryRoom + ', Mail ' + mailRoom);
+myApartment();
+
+function myApartment() {
+	var myCoffeeMaker = 'Aeropress';
+	var buildingAddress = '150 E 14th St, New York, NY';
+	var myCloset = 'Extra coats in the back';
+	var buildingLaundryCode = 4927;
+	var myRefridgerator = 'Filled with veggies and dark chocolate.';
+	var myDog = 'Nikko';
+	var buildingPhone = '(481) 516-2342';
+}
+
+var buildingLaundryCode = 4927,
+    buildingPhone = '(481) 516-2342',
+    buildingAddress = '150 E 14th St, New York, NY';
+
+// let can replace var in the function
+
+
+
+
+// Do not edit the code after this line
+console.log("**Apartment Building Information**");
+console.log("Laundry code: " + buildingLaundryCode + "\nPhone: " + buildingPhone + "\nMailing address: " + buildingAddress);
