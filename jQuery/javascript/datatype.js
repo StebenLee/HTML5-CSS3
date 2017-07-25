0bafff1b5d216c4dc220e65713162f0b78d8e0aa
JirLu <s1365789@gmail.com>
2017-07-25 16:41:51 +0800

    fixeddatatypeofjs

--- jQuery/javascript/datatype.js
+++ jQuery/javascript/datatype.js
@@ -8,7 +8,7 @@
 
    console.log(typeof "John") //String
    console.log(typeof 3.14) //Number
-   console.log(typeof Nan) //Number
+   console.log(typeof NaN) //Number  --> the result of nodejs is undefined???
    console.log(typeof false) //Boolean
    console.log(typeof [1,2,3,4]) //Object
    console.log(typeof {name: 'John', age: 34}) //Object
@@ -18,3 +18,17 @@
    console.log(typeof null) //Object
 
    //Yoy cannot use typeof to determine if a javascript object is an array or a date
+   /*The data type of Nan is number
+     The data type of an array is object
+     The data type of a date is object
+     The data type of null is object,too.
+     The data type of an undefined variable is undefined ***
+     The data type of a variable that has not been assigned value is undefined*** */ 
+
+//Javascript's Automatic Type Conversion
+5 + null    // returns 5         because null is converted to 0
+"5" + null  // returns "5null"   because null is converted to "null"
+"5" + 2     // returns 52        because 2 is converted to "2"
+"5" - 2     // returns 3*         because "5" is converted to 5
+"5" * "2"   // returns 10        because "5" and "2" are converted to 5 and 2
+"5" / "2"   // return 2.5* 
