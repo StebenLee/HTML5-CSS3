8bfb7d22ec1603a91c6e2e2cb4e5eb10c1efdf90
JirLu <s1365789@gmail.com>
2017-07-30 23:13:06 +0800

    fibmemoize

--- jQuery/javascript/memoize.js
+++ jQuery/javascript/memoize.js
@@ -20,3 +20,22 @@ memoFooBar(1);
 memoFooBar(1);
 memoFooBar(2);
 memoFooBar(2);
+
+//fib
+var memoizer = function (memo, fundamental) {
+  var shell = function (n) {
+    var result = memo[n];
+    if (typeof result !== 'number') {
+      result = fundamental(shell, n);
+      memo[n] = result;
+    }
+    return result;
+  };
+  return shell;
+};
+
+var fibonacci = memoizer([0, 1], function (shell, n) {
+  return shell(n - 1) + shell(n - 2);
+});
+
+console.log(fibonacci(10));
