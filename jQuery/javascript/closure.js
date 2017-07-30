71d05a0b1ac1fb5cc7f8ca2559a796bed0247afe
JirLu <s1365789@gmail.com>
2017-07-30 21:57:01 +0800

    newexample

--- jQuery/javascript/closure.js
+++ jQuery/javascript/closure.js
@@ -34,3 +34,48 @@ console.log(add10(2));//return 12
 console.log(add5(4));//return 9
 console.log(add5(2).x);//undefined, because the scope is different
 //Closure is like object oriented programming. It make property and function have relationship
+
+//this doesn't work
+var myObject = function () {
+  var value = 0;
+
+  return {
+    increment: function (inc) {
+      value += typeof inc === 'number' ? inc : 1;
+    },
+    getValue: function () {
+      return value;
+    }
+  };
+}();
+
+//quo function
+
+var quo = function (status) {
+  return {
+    get_status: function () {
+      return status;
+    }
+  };
+};
+// an instance of quo
+
+var myQuo = quo("amazed");
+console.log(myQuo.get_status());
+
+//The DOM practice
+
+var fade = function (node) {
+  var level = 1;
+  var step = function () {
+    var hex = level.toString(16);
+    node.style.backgroundColor = '#FFF' + hex + hex;
+    if (level < 15) {
+      level += 1;
+      setTimeout(step, 100);
+    }
+  };
+  setTimeout(step, 100);
+};
+
+//fade(document.body);
--- jQuery/javascript/functionpractice.js
+++ jQuery/javascript/functionpractice.js
@@ -36,3 +36,70 @@ console.log(myObject.value);//1
 
 myObject.increment(2);
 console.log(myObject.value);//2
+
+//Arguments
+
+var sum = function() {
+  var i, sum = 0;
+  for (i = 0; i< arguments.length; i+=1) {
+  	sum += arguments[i];
+  }
+  return sum;
+}
+
+console.log("Arguments Sum "+sum(4, 8, 15, 16, 23, 42));
+
+//Exception (throw will interupt function)
+//
+
+var add = function(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+  	throw {
+  	  name: 'TypeError',
+  	  message: 'add needs numbers'
+  	};
+  }
+  return a + b;
+}
+// if an exception is thrown within a try block, control will go to its catch clause.
+var try_it = function() {
+  try {
+    add("seven");
+  } catch(e) {
+    console.log(e.name + ' : ' + e.message);
+  }
+}
+try_it();
+
+//recursion
+var hanoi = function (disc, src, aux, dst) {
+  if (disc > 0) {
+  	hanoi(disc - 1, src, dst, aux);
+  	console.log('Move disc' + disc + ' from ' + src + ' to ' + dst);
+  	hanoi(disc - 1, aux, src, dst);
+  }
+};
+
+hanoi(3, 'Src', 'Aux', 'Dst');
+
+//Treenode
+var walk_the_DOM = function walk(node, func) {
+  func(node);
+  node = node.firstChild;
+  while (node) {
+  	walk(node, func);
+  	node=node.nextSibling;
+  }
+};
+
+var getElementsByAttribute = function (att, value) {
+  var results = [];
+
+  walk_the_DOM(document.body, function (node) {
+    var actual = node.nodeType === 1 && node.getAttribute(att);
+    if (typeof actual === 'string' && (actual === value || typeof value !== 'string')) {
+      results.push(node);
+    }
+  });
+  return results;
+}
