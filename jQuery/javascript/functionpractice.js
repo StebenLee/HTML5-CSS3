184e11f12a843f67361fe55e5588f3160f45001e
JirLu <s1365789@gmail.com>
2017-07-30 14:07:45 +0800

    Invocation Pattern

--- jQuery/javascript/functionpractice.js
+++ jQuery/javascript/functionpractice.js
@@ -23,3 +23,16 @@ takeOrder('egg', 'meaf');
 takeOrder('beef', 'mogo');
 takeOrder('slive', 'mushroom');
 console.log(getTotal());
+//----------Method invocation Pattern
+var myObject = {
+  value: 0,
+  increment: function (inc) {
+  	this.value += typeof inc === 'number' ? inc : 1;
+  }
+}
+
+myObject.increment();
+console.log(myObject.value);//1
+
+myObject.increment(2);
+console.log(myObject.value);//2
