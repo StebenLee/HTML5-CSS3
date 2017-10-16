57627a47d05413fdf1ee9f44a08f9dff3524ee68
JirLu <s1365789@gmail.com>
2017-10-17 07:19:25 +0800

    arraypractice

--- jQuery/javascript/arraypractice.js
+++ jQuery/javascript/arraypractice.js
@@ -11,3 +11,30 @@ console.log(bucketList);
 
 bucketList.pop()
 console.log(bucketList);
+
+// matrix  
+
+Array.matrix = function(m, n, initial) {
+  let a, i, j, mat = [];
+  for (i=0; i<m; i++) {
+  	a = [];
+  	for(j=0; j<n; j++) {
+  	  a[j] = initial;
+  	}
+  	mat[i]  = a;
+  }
+  return mat;
+}
+
+var mMatrix = Array.matrix(3,3,0);
+
+Array.identity = function(n) {
+  var i, mat = Array.matrix(n, n, 0);
+  for(i=0; i<n; i+=1) {
+  	mat[i][i]=1;
+  }
+  return mat;
+}
+
+mMatrix = Array.identity(4);
+console.log(mMatrix[3][3]);
