cecbb4a82feaf20cecc7370b3f215198b6cea129
JirLu <s1365789@gmail.com>
2017-10-21 13:04:04 +0800

    reselect and arraypra

--- jQuery/javascript/arraypractice.js
+++ jQuery/javascript/arraypractice.js
@@ -1,40 +1,19 @@
-var bucketList = ['how','are','you'];
-console.log(bucketList);
 
-var listItem = bucketList[2];
-console.log(listItem);
-
-console.log(bucketList.length);
-
-bucketList.push('item3', 'item4');
-console.log(bucketList);
-
-bucketList.pop()
-console.log(bucketList);
-
-// matrix  
-
-Array.matrix = function(m, n, initial) {
-  let a, i, j, mat = [];
-  for (i=0; i<m; i++) {
-  	a = [];
-  	for(j=0; j<n; j++) {
-  	  a[j] = initial;
-  	}
-  	mat[i]  = a;
-  }
-  return mat;
+function c(a) {
+	return function () {
+		return 0;
+	}
 }
 
-var mMatrix = Array.matrix(3,3,0);
-
-Array.identity = function(n) {
-  var i, mat = Array.matrix(n, n, 0);
-  for(i=0; i<n; i+=1) {
-  	mat[i][i]=1;
-  }
-  return mat;
+function abc() {
+	//console.log(1)
+const selector = function(c) {
+	console.log(6);
+	return function () {
+		return 25;
+	}
+}
+return selector;
 }
 
-mMatrix = Array.identity(4);
-console.log(mMatrix[3][3]);
+abc.apply(null,[1,2])
